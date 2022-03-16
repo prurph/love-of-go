ee515f6cf98e16e8585bebb152628907a59d3e7a
Prescott Murphy <prurph@gmail.com>
2022-03-15 22:17:22 -0400

    Implement Multiply function

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,7 +1,7 @@
 package calculator
 
 type Number interface {
-    int | int64 | float64
+    int | int64 | float32 | float64
 }
 
 func Add[T Number](a, b T) T {
@@ -10,4 +10,8 @@ func Add[T Number](a, b T) T {
 
 func Subtract[T Number](a, b T) T {
     return a - b
+}
+
+func Multiply[T Number](a, b T) T {
+    return a * b
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -22,3 +22,12 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("want %d, got %d", want, got)
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	want := 9
+	got := calculator.Multiply(3, 3)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
