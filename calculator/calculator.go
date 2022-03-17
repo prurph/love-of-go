02475557bc321386dc1f7382c8ef7d14768f0280
Prescott Murphy <prurph@gmail.com>
2022-03-17 16:40:37 -0400

    Remove generics from subtract and multiply methods and add test cases

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,17 +1,13 @@
 package calculator
 
-type Number interface {
-    int | int64 | float32 | float64
-}
-
 func Add(a, b float64) float64 {
-    return a + b
+	return a + b
 }
 
-func Subtract[T Number](a, b T) T {
-    return a - b
+func Subtract(a, b float64) float64 {
+	return a - b
 }
 
-func Multiply[T Number](a, b T) T {
-    return a * b
+func Multiply(a, b float64) float64 {
+	return a * b
 }
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -5,12 +5,13 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	a, b float64
+	want float64
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	type testCase struct {
-		a, b float64
-		want float64
-	}
 	testCases := []testCase{
 		{a: 2, b: 2, want: 4},
 		{a: 1, b: 1, want: 2},
@@ -19,25 +20,38 @@ func TestAdd(t *testing.T) {
 	for _, tc := range testCases {
 		got := calculator.Add(tc.a, tc.b)
 		if tc.want != got {
-			t.Errorf("want %f, got %f", tc.want, got)
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
 		}
 	}
 }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
-	want := 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %d, got %d", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 0},
+		{a: 2, b: 4, want: -2},
+		{a: 0, b: 2, want: -2},
+		{a: -1, b: -2, want: 1},
+	}
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
 func TestMultiply(t *testing.T) {
 	t.Parallel()
-	want := 9
-	got := calculator.Multiply(3, 3)
-	if want != got {
-		t.Errorf("want %d, got %d", want, got)
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 0, b: 2, want: 0},
+		{a: 2, b: -2, want: -4},
+	}
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want: %f, got: %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
