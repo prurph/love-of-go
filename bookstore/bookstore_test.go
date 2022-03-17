4a694fa349129b3caff42e1db0503fe6db46b3cb
Prescott Murphy <prurph@gmail.com>
2022-03-17 18:46:52 -0400

    Error on buying book with zero copies

--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -23,8 +23,24 @@ func TestBuy(t *testing.T) {
 	}
 	want := book
 	want.Copies--
-	got := bookstore.Buy(book)
+	got, err := bookstore.Buy(book)
+	if err != nil {
+		t.Fatal(err)
+	}
 	if want != got {
 		t.Errorf("Buy(%v): want %v, got %v", book, want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	book := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(book)
+	if err == nil {
+		t.Error("want error buying book with zero remaining copies, but got nil")
 	}
 }
